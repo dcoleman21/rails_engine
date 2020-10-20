f268331b338871d55d9da234cc47dab219112db6
Dani Coleman <danicoleman00@gmail.com>
2020-10-20 17:29:04 -0600

    Add routes for merchant item relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:show, :index, :create, :destroy, :update]
-      resources :merchants, only: [:show, :index, :create, :destroy, :update]
+      resources :items
+      resources :merchants
+      namespace :items do
+        get '/:id/merchant', to: 'merchants#show'
+      end
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+      end
     end
   end
 end
