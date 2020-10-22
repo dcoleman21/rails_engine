345cbb9b0f8a9e10ab901eaaead28a477019b914
Dani Coleman <danicoleman00@gmail.com>
2020-10-22 17:33:49 -0600

    Add merchant revenue action

--- app/controllers/api/v1/merchants/business_intel_controller.rb
+++ app/controllers/api/v1/merchants/business_intel_controller.rb
@@ -8,4 +8,9 @@ class Api::V1::Merchants::BusinessIntelController < ApplicationController
     merchants = MerchantFacade.most_items_sold(params[:quantity])
     render json: MerchantSerializer.new(merchants).serialized_json
   end
+
+  def merchant_revenue
+    merchant = MerchantFacade.revenue(params[:id])
+    render json: RevenueSerializer.new(merchant).serialized_json
+  end
 end
