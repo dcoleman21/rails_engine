9aaa0b219eff2a45e60d2918c4bdce045a09e439
Dani Coleman <danicoleman00@gmail.com>
2020-10-22 11:06:37 -0600

    Move most revenue method to a merchant facade

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,7 +7,3 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 end
-
-def self.most_revenue(quantity)
-  joins(:transactions).select("items.id, items.name, sum(invoice_items.unit_price * invoice_items.quantity) as revenue").where("invoices.status='shipped' AND transactions.result='success'").group("merchant.id").order("revenue DESC").limit(quantity)
-end
